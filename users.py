770f60e636e40c390d94e662b03f5eccc91b10a8
faiii <93246297+genmiao@users.noreply.github.com>
2023-12-12 11:05:17 +0800

    Update users.py

--- users.py
+++ users.py
@@ -28,3 +28,62 @@ def delete_transaction(trans_id):
     params = (trans_id,)
     result = fetchone(query, params)
     return result["trans_id"]
+
+
+## USERS
+
+def get_user(user_id):
+    query = "SELECT * FROM get_users WHERE user_id = %s"
+    params = (user_id,)
+    result = fetchone(query, params)
+    return result
+
+def create_user(username, password, email):
+    query = "CALL create_user(%s, %s, %s)"
+    params = (username, password, email)
+    result = fetchone(query, params)
+    return result
+
+def update_user(user_id, new_username, new_password, new_email):
+    query = "CALL update_user(%s, %s, %s, %s)"
+    params = (user_id, new_username, new_password, new_email)
+    result = fetchone(query, params)
+    return result
+
+def delete_user(user_id):
+    query = "CALL delete_user(%s)"
+    params = (user_id,)
+    result = fetchone(query, params)
+    return result
+
+
+## CATEGORIES
+
+def create_category(cat_username):
+    query = "CALL create_category(%s)"
+    params = (cat_username,)
+    result = fetchone(query, params)
+    return result["cat_id"]
+
+def get_categories():
+    query = "SELECT * FROM get_categories"
+    result = fetchall(query)
+    return result
+
+def get_category(cat_id):
+    query = "SELECT * FROM get_categories WHERE cat_id = %s"
+    params = (cat_id,)
+    result = fetchone(query, params)
+    return result
+
+def update_category(cat_id, cat_username):
+    query = "CALL update_category(%s, %s)"
+    params = (cat_id, cat_username)
+    result = fetchone(query, params)
+    return result["cat_id"]
+
+def delete_category(cat_id):
+    query = "CALL delete_category(%s)"
+    params = (cat_id,)
+    result = fetchone(query, params)
+    return result["cat_id"]
